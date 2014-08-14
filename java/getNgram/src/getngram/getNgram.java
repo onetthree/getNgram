02a802f836b72783c0654f8e4713fce53bdd3e25
ryosuke-m <ryosule.mitani@gmail.com>
2014-08-14 23:59:14 +0900

    refactored2 modify some function

--- java/getNgram/src/getngram/getNgram.java
+++ java/getNgram/src/getngram/getNgram.java
@@ -100,60 +100,7 @@ public class getNgram {
 		
 		Map<String,Integer> wordMap = this.makeWordMap(NgramVector_plane);
 		this.word_vector_map = wordMap;
-		//this.showMapContents();
-			
-			/*
-			List<String> stringListBuffer = new ArrayList<String>();
-			stringListBuffer.addAll(Arrays.asList(input_sentence_i.split(" ")));
-			stringListBuffer.add(0, "<BOS>");
-			stringListBuffer.add("<EOS>");
-			
-			//make ngram
-			List<String> ngram_vector = new ArrayList<String>();
-			for (int o_i=0;o_i<stringListBuffer.size()-(this.num-1);o_i++){
-				String ngram = new String();
-				for(int i_i=o_i;i_i<o_i+this.num;i_i++){
-					if(ngram.isEmpty()){
-						ngram=stringListBuffer.get(i_i);
-					}else{
-						ngram+="_"+stringListBuffer.get(i_i);
-					}
-				}
-				ngram_vector.add(ngram);
-			}
-			word_vector_docs.add(ngram_vector);
-		}
-		
-		//make n-gram plane vector
-		for(List<String> ngram_vector_i : word_vector_docs){
-			for(String ngram_i : ngram_vector_i){
-				ngram_vector_plane.add(ngram_i);
-			}
-		}
 
-		//Map<String,int> word_map = new HashMap<String,int>();
-		//Map word_map<String,Integer> = new HashMap<String,Integer>();
-		Map <String,Integer>word_map = new HashMap<String,Integer>();
-		//Map<String,String> mp = new HashMap<String,String>();
-		
-		for(String word_i : ngram_vector_plane){
-			if (word_map.containsKey(word_i)){
-				int new_num = (int)word_map.get(word_i)+1;
-				word_map.put(word_i, new_num);
-			}else{
-				word_map.put(word_i, 1);
-			}
-		}
-
-		//dbg
-		//for(Set<String,int> wv : word_map.entrySet()){
-		for(Map.Entry<String,Integer> wv : word_map.entrySet()){
-			System.out.println(wv.getKey()+":"+wv.getValue());
-		}
-
-		this.word_vector = word_vector;
-		this.word_vector_map = word_map;
-*/
 		return true;
 	}
 	
--- java/getNgram/src/getngram/main.java
+++ java/getNgram/src/getngram/main.java
@@ -17,16 +17,35 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-	    String[] input_stirng = {
-		    "This is a pen .",
-		    "This is an egg .",
-		    "That person plays a song with his piano .",
-		    "His name was Tanaka , It was one of the most famous name in Japan ."
-		};
 	    
-	    getNgram p = new getNgram(input_stirng, 2);
-	    System.out.println(p.parseSentence());
+	//input sentences
+	final String[] input_stirng = {
+		"This is a pen .",
+		"This is an egg .",
+		"That person plays a song with his piano .",
+		"His name was Tanaka , It was one of the most famous name in Japan ."
+	    };
+	
+	//get n-gram map and show it.
+	
+	//unigram
+	System.out.println("---unigram---");
+	getNgram p1 = new getNgram(input_stirng, 1);
+	//if the analysis is complete, it show you  a content of the map container.
+	if(p1.parseSentence()) p1.showMapContents();
+	
+	//bigram
+	System.out.println("---bigram---");
+	getNgram p2 = new getNgram(input_stirng, 2);
+	//if the analysis is complete, it show you  a content of the map container.
+	if(p2.parseSentence()) p2.showMapContents();
 
+	
+	//trigram
+	System.out.println("---trigram---");
+	getNgram p3 = new getNgram(input_stirng, 3);
+	//if the analysis is complete, it show you  a content of the map container.
+	if(p3.parseSentence()) p3.showMapContents();
     }
     
 }
