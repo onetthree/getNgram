4d011e2780b206aa2784165284f4e58dfabf3946
ryosuke-m <ryosule.mitani@gmail.com>
2014-08-14 23:31:54 +0900

    add some function

--- java/getNgram/src/getngram/getNgram.java
+++ java/getNgram/src/getngram/getNgram.java
@@ -31,7 +31,57 @@ public class getNgram {
 	public void setSentence(String Sentence){
 		//this.input_sentence = Sentence;
 	}
-
+	
+	private List<String> String2WordVector(String input_sentence_i){
+		
+		List<String> stringListBuffer = new ArrayList<String>();
+		
+		stringListBuffer.addAll(Arrays.asList(input_sentence_i.split(" ")));
+		stringListBuffer.add(0, "<BOS>");
+		stringListBuffer.add("<EOS>");
+		
+		return stringListBuffer;
+	}
+	
+	private List<String> makeNgram(List<String> wordVector){
+		
+		List<String> ngram_vector = new ArrayList<String>();
+		
+		for (int o_i=0;o_i<wordVector.size()-(this.num-1);o_i++){
+			String ngram = new String();
+			for(int i_i=o_i;i_i<o_i+this.num;i_i++){
+				if(ngram.isEmpty()){
+					ngram=wordVector.get(i_i);
+				}else{
+					ngram+="_"+wordVector.get(i_i);
+				}
+			}
+			ngram_vector.add(ngram);
+		}	
+		return ngram_vector;
+	}
+	
+	private Map<String,Integer> makeWordMap(List<String> ngram_vector_plane){
+		Map <String,Integer>word_map = new HashMap<String,Integer>();
+		
+		for(String word_i : ngram_vector_plane){
+			if (word_map.containsKey(word_i)){
+				int new_num = (int)word_map.get(word_i)+1;
+				word_map.put(word_i, new_num);
+			}else{
+				word_map.put(word_i, 1);
+			}
+		}
+		
+		return word_map;
+	}
+	
+	public void showMapContents(){
+		for(Map.Entry<String,Integer> wv : this.word_vector_map.entrySet()){
+			System.out.println(wv.getKey()+":"+wv.getValue());
+		}
+	}
+	
 	public boolean parseSentence(){
 		if(this.input_sentence.length==0 || this.num<1){
 			System.out.println("type correct params.");
@@ -64,7 +114,7 @@ public class getNgram {
 			word_vector_docs.add(ngram_vector);
 		}
 		
-		//make n-gram
+		//make n-gram plane vector
 		for(List<String> ngram_vector_i : word_vector_docs){
 			for(String ngram_i : ngram_vector_i){
 				ngram_vector_plane.add(ngram_i);
